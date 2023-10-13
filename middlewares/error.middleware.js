f2f54765581b17bc00a04f0e039862c9d1bcd2ce
adiMallya <55098608+adiMallya@users.noreply.github.com>
2023-10-13 06:09:32 +0000

    add: common error handler

---
+++ middlewares/error.middleware.js
@@ -0,0 +1,36 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+// @desc: handle any error responses not taken care by defined routes
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  error.message = err.message;
+  // Log to console for dev
+  console.log(err.stack);
+
+  // Mongoose bad ObjectId
+  if (err.name === "CastError") {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose validation
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+  
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message,
+  });
+
+};
+
+module.exports = errorHandler;
